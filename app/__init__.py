5ed5c2be251080e8c43cebaf8843b275017385e9
Gabriel <egitoo@gmail.com>
2020-03-03 20:47:50 -0400

    aula02

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://suporte:$uportE99@localhost/atividades'
+app.config['SQLALCHEMY_EACHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+from app.models.tables import Usuario
+from app.models.tables import Atividade
 
 
 @app.route('/home')
 def home():
-    title = "Presunto viado"
-    lista = ['curso do gordo bixa', 'curso do leitao baitola']
-    return render_template('index.html', titulo=title, eventos=lista)
+    title = "Ikaro viado"
+    usuarios = Usuario.query.all()
+    return render_template('index.html', titulo=title, usuarios=usuarios)
 
 
